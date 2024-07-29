7b6666b91e3d6f1f968dc01990f7370ab003dcbd
nagisora <nagisora.icanspeng@gmail.com>
2024-07-29 22:24:57 +0900

    初回コミット

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import './App.css'
+import { useState } from 'react'
+import { Todo } from './Todo';
+import { TodoType } from "./types/todo"
+
+function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    })
+  }
+  return (
+    <>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
+      ))}
+    </>
+  )
+}
+
+export default App
