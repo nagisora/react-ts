fdf94d7a73242b7cd39663f596bf2b91bedafebe
nagisora <nagisora.icanspeng@gmail.com>
2024-07-30 20:25:45 +0900

    カスタムフック利用

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import axios from 'axios'
 import './App.css'
-import { useState } from 'react'
 import { Text } from './Text';
 import { UserCard } from './components/UserCard';
-import { UserProfile } from './types/userProfile';
-import { User } from './types/api/user';
+import { useAllUsers } from './hooks/useAllUsers';
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      }).catch(() => {
-        setError(true);
-      }).finally(() => {
-        setLoading(false);
-      });
-  }
+  const onClickFetchData = () => getUsers();
+  
   return (
     <>
       
