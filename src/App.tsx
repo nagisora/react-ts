0cc4a1ca8848099563dfb1aa03ea3c148a1d476d
nagisora <nagisora.icanspeng@gmail.com>
2024-07-30 18:17:01 +0900

    sec10まで修了

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import './App.css'
 import { useState } from 'react'
 import { Todo } from './Todo';
 import { TodoType } from "./types/todo"
+import { Text } from './Text';
+import { UserProfile } from './UserProfile';
+import { User } from './types/user';
+
+const user: User = {
+  name: "なぎそら",
+  // hobbies: ["アニメ", "ゲーム"],
+}
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -14,6 +22,8 @@ function App() {
   }
   return (
     <>
+    <UserProfile user={user} />
+      <Text color="red" fontSize='18px' />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
